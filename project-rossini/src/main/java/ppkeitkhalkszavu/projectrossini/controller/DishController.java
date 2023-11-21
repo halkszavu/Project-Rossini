f529b29d29b437ad1518d4ab6f1942093765b98a
Botond Lajos Perényi <halkszavu@outlook.com>
2023-11-21 20:56:43 +0100

    added swagger it doesn't work for some reason. Look into it.

--- project-rossini/src/main/java/ppkeitkhalkszavu/projectrossini/controller/DishController.java
+++ project-rossini/src/main/java/ppkeitkhalkszavu/projectrossini/controller/DishController.java
@@ -1,5 +1,9 @@
 package ppkeitkhalkszavu.projectrossini.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +12,7 @@ import ppkeitkhalkszavu.projectrossini.repository.DishRepository;
 
 import java.util.List;
 
+@Tag(name = "Dish")
 @RestController
 public class DishController {
 
@@ -18,6 +23,11 @@ public class DishController {
         this.dishRepository = dishRepository;
     }
 
+    @Operation(summary = "Get all dishes")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found all dishes"),
+            @ApiResponse(responseCode = "404", description = "Dishes not found")
+    })
     @GetMapping("/dishes")
     public List<Dish> getDishes() {
         return dishRepository.getDishes();

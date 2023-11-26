a6a1e7d9caefed7ad005bc470f7fd36fe02415f3
Botond Lajos Perényi <halkszavu@outlook.com>
2023-11-26 19:27:49 +0100

    added substitution for wine and chianti

--- project-rossini/src/main/resources/db/migration/V2__insert_data.sql
+++ project-rossini/src/main/resources/db/migration/V2__insert_data.sql
@@ -19,6 +19,7 @@ INSERT INTO dishes (name, user_id) SELECT 'Tuna sweetcorn tagliatelle', id FROM 
 INSERT INTO dishes (name, user_id) SELECT 'Sausage pappardelle', id FROM users WHERE name = 'Lewis';
 
 INSERT INTO recipes (name, method_time, serves, method_descr, dish_id) SELECT 'Tuna sweetcorn tagliatelle', 15, 1, 'Method', id FROM dishes WHERE name = 'Tuna sweetcorn tagliatelle';
+INSERT INTO recipes (name, method_time, serves, method_descr, dish_id) SELECT 'Sausage pappardelle', 15, 1, 'Method', id FROM dishes WHERE name = 'Sausage pappardelle';
 
 INSERT INTO ingredient (amount, material_id, recipe_id) SELECT 125, id FROM materials WHERE name = 'Tagliatelle pasta', id FROM recipes WHERE name = 'Tuna sweetcorn tagliatelle';
 INSERT INTO ingredient (amount, prep_descr, prep_time, material_id, recipe_id) SELECT 10, 'grated', 2, id FROM materials WHERE name = 'Parmesan cheese', id FROM recipes WHERE name = 'Tuna sweetcorn tagliatelle';
@@ -36,3 +37,5 @@ INSERT INTO ingredient (amount, material_id, recipe_id) SELECT 100, id FROM mate
 INSERT INTO ingredient (amount, material_id, recipe_id) SELECT 100, id FROM materials WHERE name = 'Red wine', id FROM recipes WHERE name = 'Sausage pappardelle';
 INSERT INTO ingredient (amount, material_id, recipe_id) SELECT 0.5, id FROM materials WHERE name = 'Sprigs of parsley', id FROM recipes WHERE name = 'Sausage pappardelle';
 INSERT INTO ingredient (amount, material_id, recipe_id) SELECT 75, id FROM materials WHERE name = 'Saugsages', id FROM recipes WHERE name = 'Sausage pappardelle';
+
+INSERT INTO substitutions (source_id, substitute_id) SELECT id FROM ingredient WHERE name = 'Chianti' AND recipe_id = (SELECT id FROM recipes WHERE name = 'Sausage pappardelle'), id FROM ingredient WHERE name = 'Red wine' AND recipe_id = (SELECT id FROM recipes WHERE name = 'Sausage pappardelle');

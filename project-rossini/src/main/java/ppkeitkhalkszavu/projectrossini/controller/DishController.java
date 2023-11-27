3eae8fa4fb8fd262d89cfd7e97fb7469cf654583
Botond Lajos Perényi <halkszavu@outlook.com>
2023-11-27 17:18:00 +0100

    DishController updated to use DishRepository

--- project-rossini/src/main/java/ppkeitkhalkszavu/projectrossini/controller/DishController.java
+++ project-rossini/src/main/java/ppkeitkhalkszavu/projectrossini/controller/DishController.java
@@ -6,10 +6,14 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ppkeitkhalkszavu.projectrossini.domain.Dish;
-import ppkeitkhalkszavu.projectrossini.repository.MockDishRepository;
+import ppkeitkhalkszavu.projectrossini.repository.DishRepository;
 
 import java.util.List;
 
@@ -18,21 +22,29 @@ import java.util.List;
 @RestController
 public class DishController {
 
-    private final MockDishRepository dishRepository;
+    private final DishRepository dishRepository;
 
     @Autowired
-    public DishController(MockDishRepository dishRepository) {
+    public DishController(DishRepository dishRepository) {
         this.dishRepository = dishRepository;
     }
 
-    @Operation(summary = "Get all dishes")
+    @Operation(summary = "Get all dishes with pagination")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Found all dishes"),
-            @ApiResponse(responseCode = "404", description = "Dishes not found")
+            @ApiResponse(responseCode = "400", description = "Invalid url parameters supplied"),
     })
     @GetMapping("/dishes")
-    public List<Dish> getDishes() {
-        log.info("Calling GET /dishes endpoint");
-        return dishRepository.getDishes();
+    public List<Dish> getDishes(@RequestParam(required = false, defaultValue = "100") Integer limit, @RequestParam(required = false, defaultValue = "desc") String sort) {
+        log.info("Calling GET /dishes endpoint with limit: {} and sort: {}", limit, sort);
+
+        if(!sort.equals("desc") && !sort.equals("asc")) {
+            throw new IllegalArgumentException("Invalid sort parameter supplied");
+        }
+
+        var sortParam = sort.equalsIgnoreCase("desc") ? Sort.by("id").descending() : Sort.by("id").ascending();
+        Page<Dish> dishes = dishRepository.findAll(PageRequest.of(0, limit, sortParam));
+
+        return dishes.toList();
     }
 }
--- project-rossini/src/main/java/ppkeitkhalkszavu/projectrossini/repository/DishRepository.java
+++ project-rossini/src/main/java/ppkeitkhalkszavu/projectrossini/repository/DishRepository.java
@@ -1,8 +1,10 @@
 package ppkeitkhalkszavu.projectrossini.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ppkeitkhalkszavu.projectrossini.domain.Dish;
 
 public interface DishRepository extends JpaRepository<Dish, Integer> {
-    
+    Page<Dish> findAll(Pageable pageable);
 }

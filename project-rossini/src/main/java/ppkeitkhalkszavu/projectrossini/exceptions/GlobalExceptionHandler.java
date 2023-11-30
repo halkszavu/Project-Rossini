00d5b5741612062419f69987d60d661acc7f0b8e
Botond Lajos Perényi <halkszavu@outlook.com>
2023-11-30 12:51:47 +0100

    Exceptions now return with Status code

---
+++ project-rossini/src/main/java/ppkeitkhalkszavu/projectrossini/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package ppkeitkhalkszavu.projectrossini.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e){
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}

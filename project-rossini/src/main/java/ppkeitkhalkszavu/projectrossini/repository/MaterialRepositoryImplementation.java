8485a3d04c875c2fa1a65967c8e2e7caa86609f4
Botond Lajos Perényi <halkszavu@outlook.com>
2023-11-30 09:42:27 +0100

    MaterialRepository done.

---
+++ project-rossini/src/main/java/ppkeitkhalkszavu/projectrossini/repository/MaterialRepositoryImplementation.java
@@ -0,0 +1,47 @@
+package ppkeitkhalkszavu.projectrossini.repository;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import ppkeitkhalkszavu.projectrossini.domain.Benefit;
+import ppkeitkhalkszavu.projectrossini.domain.Material;
+import ppkeitkhalkszavu.projectrossini.domain.UnitOfMeasure;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class MaterialRepositoryImplementation implements MaterialRepository{
+
+    private EntityManager entityManager;
+    private final BenefitRepository benefitRepository;
+
+    @Override
+    public Material save(String name, UnitOfMeasure unit) {
+        Material material = new Material();
+        material.setName(name);
+        material.setUnit(unit);
+        entityManager.persist(material);
+        return material;
+    }
+
+    @Override
+    public Material setBenefit(int id, int benefitId) {
+        Optional<Benefit> benefit = benefitRepository.findById(benefitId);
+        if (benefit.isEmpty())
+            throw new NoSuchElementException("Benefit with id " + benefitId + " not found");
+        else{
+            Optional<Material> material = entityManager.createQuery("SELECT m FROM Material m WHERE m.id = :id", Material.class)
+                    .getResultStream()
+                    .findFirst();
+            if(material.isEmpty())
+                throw new NoSuchElementException("Material with id " + id + " not found");
+            else{
+                material.get().getBenefit().add(benefit.get());
+                entityManager.persist(material.get());
+                return material.get();
+            }
+        }
+    }
+}
